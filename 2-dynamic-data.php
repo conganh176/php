19c2cb0aa5468761ceb5ce1348e53099da1501ac
conganh176 <40571925+conganh176@users.noreply.github.com>
2021-01-08 09:37:44 +0700

    .

---
+++ 2-dynamic-data.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		$title = 'Cong Anh Nguyen';
+	?>
+
+	<h1>Hello <?php echo $title; ?></h1>
+</body>
+</html>
---
+++ 3-var.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+$name = 'Cong Anh Nguyen';
+$number = 420;
+$NUMBER = 69;
+$float = 420.69;
+
+echo $number . "." . $NUMBER;
+
+$html_data = "<h1>Hello World</h1>";
+echo $html_data;
+?>
+</body>
+</html>
