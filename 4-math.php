5fb66eef0f161944b2410743da3ae3e9ce7e7cdf
conganh176 <40571925+conganh176@users.noreply.github.com>
2021-01-10 13:27:39 +0700

    .

--- 4-math.php
+++ 4-math.php
@@ -5,7 +5,18 @@
 </head>
 <body>
 <?php 
-echo 420 + 69
+echo 420 + 69;
+echo "<br>";
+echo 420 - 69;
+echo "<br>";
+echo 420 * 69;
+echo "<br>";
+echo 420 / 69;
+
+$number1 = 69;
+$number2 = 420;
+echo "<br>";
+echo $number1 / $number2 
 ?>
 </body>
 </html>
---
+++ 5-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+$numbers = [420, 69, 4, 2, 0, 6, 9];
+
+echo $numbers[0];
+echo '<br>';
+print_r($numbers);
+
+$info = ['first_name' => 'Anh', 'last_name' => 'Nguyen Cong'];
+echo '<br>';
+print_r($info);
+echo '<br>' . $info['first_name']
+?>
+</body>
+</html>
