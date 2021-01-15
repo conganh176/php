c0d4b2b5d19a852160145cbdc5463ba4426a59d0
conganh176 <40571925+conganh176@users.noreply.github.com>
2021-01-15 11:44:07 +0700

    .

---
+++ 21-sessions/session-1.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+$_SESSION['greetings'] = "Welcome";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sessions</title>
+</head>
+<body>
+<?php
+    
+?>
+</body>
+</html>
---
+++ 21-sessions/session-2.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+echo $_SESSION['greetings'];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sessions</title>
+</head>
+<body>
+<?php
+    
+?>
+</body>
+</html>
